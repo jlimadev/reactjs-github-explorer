7a0a6ddd786729921d5daf0f14d1f02f101f0186
jlimadev <jlima.dev@gmail.com>
2020-09-04 10:32:01 -0300

    implemented: dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,67 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositories on GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories on Github</Title>
+
+      <Form>
+        <input placeholder="Type the repository name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/67848049?s=460&u=a87c5c1a51f39c2b989480dc173550bdfa692619&v=4"
+            alt="Jonathan"
+          />
+          <div>
+            <strong>gostack-nodejs-typeorm-db-challenge</strong>
+            <p>
+              Gostack challenge - Using typescript, typeorm, database (postgres
+              running on docker) and file upload.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/67848049?s=460&u=a87c5c1a51f39c2b989480dc173550bdfa692619&v=4"
+            alt="Jonathan"
+          />
+          <div>
+            <strong>gostack-nodejs-typeorm-db-challenge</strong>
+            <p>
+              Gostack challenge - Using typescript, typeorm, database (postgres
+              running on docker) and file upload.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/67848049?s=460&u=a87c5c1a51f39c2b989480dc173550bdfa692619&v=4"
+            alt="Jonathan"
+          />
+          <div>
+            <strong>gostack-nodejs-typeorm-db-challenge</strong>
+            <p>
+              Gostack challenge - Using typescript, typeorm, database (postgres
+              running on docker) and file upload.
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
